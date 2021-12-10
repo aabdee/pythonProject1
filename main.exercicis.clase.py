d5f115bedd03092b0a02f3b0a7119f3503c422cb
aabdee <aouachikh@institutmvm.cat>
2021-12-10 10:50:09 +0100

    Ultima modificación: hay que imprimir tabla, codi1, codi2, codi3

--- main.exercicis.clase.py
+++ main.exercicis.clase.py
@@ -1,11 +1,49 @@
 
+# ejercicio 1 practica 10
+def main():
+
+    n1 = int(input("Introdueix el primer nombre enter: "))
+    n2 = int(input("Introdueix el segon nombre enter: "))
+
+    if n1 < n2:
+        while n1 < n2+1:
+            print(n1)
+            n1+=1
+    else:
+        while n1 > n2:
+            print("Error")
+            n1 = int(input("Introdueix el primer nombre enter: "))
+            n2 = int(input("Introdueix el segon nombre enter: "))
+        while n1 < n2+1:
+            print(n1)
+            n1=+1
+
+if __name__ == '__main__':
+    main()
+
+
+#ejercicio 3 practica 10
 
 def main():
-    cadena = "Practica els problemes de list comprehensions per a ser més Pythonic!"
 
-    nom = [i if i < 5 else 0 for i in cadena]
+    persona={}
+    bbdd={}
+    contador=0
+    numero_usuaris=int(input("Introdueix el numero d'usuaris que vols inserir: "))
 
-    print(nom)
+    while contador<numero_usuaris:
+        print("Introdueix l'usuari: ")
+        persona["Nom"]=input("Introdueix el nom: ")
+        persona["departament"] = input("Introdueix el departament: ")
+        persona['clase']=0
+        while persona['clase']<1 or persona ['clase']>15:
+            persona['clase']=int(input("Introdueix la clase entre el 1 i el 15: "))
+            bbdd[contador]=persona
+            contador=+1
+    print("Els usuaris introduits són: ")
+
+    print()
+    print(bbdd)
 
 if __name__ == '__main__':
     main()
